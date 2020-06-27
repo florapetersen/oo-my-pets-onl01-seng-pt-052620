0e3758ebd443af03ff28ba636d92b922a0669a69
Flora Petersen <floramarina@comcast.net>
2020-06-27 19:33:07 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -26,6 +26,11 @@ class Owner
   end
 
   def cats #returns collection of all cats belonging to the owner
-    Cat.all.select {|cat| cat.owner == self}
+    Cat.all.select {|cat| cat.owner == self} #iterates through ALL cats and selects only the owners
+    #where self is the owner
+  end
+
+  def dogs #returns collection of all dogs belonging to the owner
+    Dog.all.select {|dog| dog.owner == self}
   end
 end
