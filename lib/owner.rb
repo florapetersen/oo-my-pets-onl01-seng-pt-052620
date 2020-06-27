d145725daf7a364bc00f217a8520593925a4747a
Flora Petersen <floramarina@comcast.net>
2020-06-27 19:48:06 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -51,8 +51,8 @@ class Owner
   end
 
   def sell_pets
-    Cat.all.each {|cat| cat.mood = "nervous" if cat.owner == self}
-    Dog.all.each {|dog| dog.mood = "nervous" if dog.owner == self}
+    Cat.all.each {|cat| cat.mood = "nervous" && cat.owner = nil if cat.owner == self}
+    Dog.all.each {|dog| dog.mood = "nervous" && dog.owner = nil if dog.owner == self}
   end
 
 
