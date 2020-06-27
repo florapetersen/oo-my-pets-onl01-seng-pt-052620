bd40b9a0454387bf706ef59f248ceabdb8a17c2c
Flora Petersen <floramarina@comcast.net>
2020-06-27 19:53:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -66,7 +66,7 @@ class Owner
     end
 
     def list_pets
-      puts "I have #{self.dogs.count} dogs and #{self.cats.count} cats."
+      puts "I have #{self.dogs.count} dog(s) and #{self.cats.count} cat(s)."
     end
   end
 
